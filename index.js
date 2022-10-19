3094bb387990a2503ed93027565b8d919641a820
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2022-10-18 23:32:17 -0300

    Tentativa de concerto

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ do{
     
     //estrutura para adicionar ou retirar o dinheiro
     switch(option){
-        case "1":
+        case 1:
             value = parseFloat(prompt("Qual o valor a ser adicionado?"))
             avaliableMoney += value
             break
-        case "2":
+        case 2:
             value = parseFloat(prompt("Qual o valor a ser retirado?"))
             avaliableMoney -= value
             break
-        case "3":
+        case 3:
             alert("Programa encerrado")
             break
         default:
@@ -25,4 +25,4 @@ do{
     }
 
     
-} while(option !== "3")
+} while(option !== 3)
